ec5727f1c9f9c47fb6705d30f0107c1c2b870a91
RodrigoMarques <rodrigomarques328@hotmail.com>
2022-12-20 15:26:41 -0300

    estudos-fetchAPI-axios

--- main.js
+++ main.js
@@ -1,12 +1,86 @@
+// ------------------------------------------------------------------------
+// Método GET:
 const url = 'http://localhost:5500/api'
 
+
+// function getUsers() {
+//     axios.get(url)
+//     .then(response => {
+//         console.log(response.data)
+//         apiResult.textContent = JSON.stringify(response.data)
+//     })
+//     .catch(error => console.error(error))
+// }
+
+// getUsers()
+
+// --------------------------------------------------------------------
+// Método POST (inserção de dados na API através do AXIOS)
+
+// function getUsers() {
+//     axios.get(url)
+//     .then(response => {
+//         console.log(response.data)
+//         apiResult.textContent = JSON.stringify(response.data)
+//     })
+//     .catch(error => console.error(error))
+// }
+
+// function addNewUser() {
+//     axios.post(url)
+//     .then(response => {
+//         console.log(response)
+//     })
+//     .catch(error => console.error(error))
+// }
+
+// const newUser = {
+//     name: "Rodrigo Marques", 
+//     avatar: "https://picsum.photos/200/300", 
+//     city: "Maceió",
+// }
+
+// getUsers()
+// addNewUser(newUser)
+
+// --------------------------------------------------------------------
+// GET com parâmetros (usuários específicos)
 
 function getUsers() {
     axios.get(url)
     .then(response => {
+        console.log(response.data)
         apiResult.textContent = JSON.stringify(response.data)
     })
     .catch(error => console.error(error))
 }
 
+function addNewUser() {
+    axios.post(url)
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => console.error(error))
+}
+function getUser() {
+    axios.get('http://localhost:5500/api/3')
+    .then(response => {
+        const data = response.data
+        userName.textContent = data.name
+        userCity.textContent = data.city
+        userID.textContent = data.id
+        userAvatar.src = data.avatar
+    })
+    .catch(error => console.error(error))
+}
+
+
+const newUser = {
+    name: "Rodrigo Marques", 
+    avatar: "https://picsum.photos/200/300", 
+    city: "Maceió",
+}
+
 getUsers()
+addNewUser(newUser)
+getUser()
